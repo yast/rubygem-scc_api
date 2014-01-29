349fefde605778f9a4c2df19ac18fb170a5b1bbb
Josef Reidinger <jreidinger@suse.cz>
2014-01-29 22:05:46 +0100

    make test location agnostic

--- test/hw_detection_test.rb
+++ test/hw_detection_test.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env rspec
 
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
 require "scc_api/hw_detection"
+
+def fixtures_dir
+  File.expand_path("../fixtures", __FILE__)
+end
 
 describe ".cpu_sockets" do
   it "returns CPU sockets number" do
-    SccApi::HwDetection.should_receive(:'`').with("lscpu").and_return(File.read("test/fixtures/lscpu_1_socket.out"))
+    SccApi::HwDetection.should_receive(:'`').with("lscpu").and_return(File.read("#{fixtures_dir}/lscpu_1_socket.out"))
     returned = SccApi::HwDetection.cpu_sockets
     expect(returned).to eq(1), "Detected CPU sockets: '#{returned}'"
   end
@@ -18,13 +24,13 @@ end
 
 describe ".graphics_card_vendors" do
   it "returns graphics cards vendors" do
-    SccApi::HwDetection.should_receive(:'`').with("lspci -n -vmm").and_return(File.read("test/fixtures/lspci_intel_gfx.out"))
+    SccApi::HwDetection.should_receive(:'`').with("lspci -n -vmm").and_return(File.read("#{fixtures_dir}/lspci_intel_gfx.out"))
     returned = SccApi::HwDetection.graphics_card_vendors
     expect(returned).to eq(["intel"]), "Detected graphics cards vendors: '#{returned}'"
   end
 
   it "returns no graphics cards vendors" do
-    SccApi::HwDetection.should_receive(:'`').with("lspci -n -vmm").and_return(File.read("test/fixtures/lspci_no_gfx.out"))
+    SccApi::HwDetection.should_receive(:'`').with("lspci -n -vmm").and_return(File.read("#{fixtures_dir}/lspci_no_gfx.out"))
     returned = SccApi::HwDetection.graphics_card_vendors
     expect(returned).to eq([]), "Detected graphics cards vendors: '#{returned}'"
   end
