45c2f630ff8b06e1b7570b8568f42626e3d8f097
Ladislav Slezák <lslezak@suse.cz>
2014-02-05 11:13:19 +0100

    use coveralls for on-line code coverage reporting at Travis CI

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,11 +1,24 @@
 
 # this needs to be at the very top of the file!!
 begin
-  require 'simplecov'
+  require "simplecov"
+
   # ignore coverage in tests
   SimpleCov.add_filter "/test/"
   # less than 60% code covarage triggers a failure
   SimpleCov.minimum_coverage 60
+
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
+
+  # start code coverage
   SimpleCov.start
 rescue LoadError
   $stderr.puts "Code coverage disabled: simplecov gem not installed"
