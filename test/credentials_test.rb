15b7cdd2ae0f0f78afbc5264e3ef61af6fd148eb
Ladislav Slezák <lslezak@suse.cz>
2014-02-05 13:17:37 +0100

    added tests for SccApi::Credentials#write

--- test/credentials_test.rb
+++ test/credentials_test.rb
@@ -18,6 +18,39 @@ describe SccApi::Credentials do
     end
   end
 
+  describe "#write" do
+    it "creates a credentials file accessible only by user" do
+      begin
+        file = "SLES_credentials"
+        credentials = SccApi::Credentials.new("name", "1234", "SLES")
+        expect {credentials.write(".")}.not_to raise_error
+
+        # the file is not empty
+        expect(File.size(file) > 0).to eq true
+        # standard file with "rw-------" permissions
+        expect(File.stat(file).mode).to eq 0100600
+      ensure
+        File.delete(file) if File.exist?(file)
+      end
+    end
+
+    it "the written file can be read back" do
+      begin
+        file = "SLES_credentials"
+        credentials = SccApi::Credentials.new("name", "1234", "SLES")
+        credentials.write(".")
+
+        read_credentials = SccApi::Credentials.read(file)
+        # the read credentials are exactly the same as written
+        expect(read_credentials.username).to eq credentials.username
+        expect(read_credentials.password).to eq credentials.password
+        expect(read_credentials.service).to eq credentials.service
+      ensure
+        File.delete(file) if File.exist?(file)
+      end
+    end
+  end
+
   describe "#to_s" do
     it "does not serialize password (to avoid logging it)" do
       password = "*eiW0yie2*"
