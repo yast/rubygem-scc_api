75b6a90b33730d6f2fe1aaa04ab0f177100d3644
Ladislav Slezák <lslezak@suse.cz>
2014-04-14 14:28:31 +0200

    return also addon version and architecture
    when querying for available addons
    
    - 0.2.9

--- lib/scc_api/product_extensions.rb
+++ lib/scc_api/product_extensions.rb
@@ -11,7 +11,8 @@ module SccApi
     def self.from_hash(extensions_response)
       extensions = extensions_response.map do |extension|
         # We do not currently have long_name or description in the SCC response
-        Extension.new(extension["zypper_name"], extension["name"], extension["architecture_id"], free: extension["free"])
+        Extension.new(extension["zypper_name"], extension["name"],
+          extension["arch"], extension["zypper_version"], free: extension["free"])
       end
 
       ProductExtensions.new(extensions)
@@ -22,15 +23,16 @@ module SccApi
   class Extension
 
     # TODO FIXME: missing dependencies
-    attr_reader :short_name, :long_name, :description, :product_ident, :free, :arch
+    attr_reader :short_name, :long_name, :description, :product_ident, :free, :arch, :version
 
-    def initialize(product_ident, short_name, arch, long_name: "", description: "", free: false)
+    def initialize(product_ident, short_name, arch, version, long_name: "", description: "", free: false)
       @arch = arch
       @short_name = short_name
       @long_name = long_name
       @description = description
       @product_ident = product_ident
       @free = free
+      @version = version
     end
   end
 end
